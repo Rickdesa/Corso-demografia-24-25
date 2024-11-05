e774b335734ed3a593db8d38e4be5177e6303ec7
Rickdesa <riccardo.desantis.1@phd.unipd.it>
2024-11-05 18:21:06 +0100

    Update Funzioni_corso_demografia_24-25.R

--- Funzioni_corso_demografia_24-25.R
+++ Funzioni_corso_demografia_24-25.R
@@ -32,3 +32,30 @@ lexis_polygon_dfcont<-function(df,max_alpha=NULL){if(is.null(max_alpha)){max_alp
   alphas<-alphas/max_alpha
   y[is.na(y)]<-max(y, na.rm = T)+5
   return(data.frame(group,x,y,alphas))}
+
+
+
+
+
+leslie_matrix_new<-function(dataframe,lifetable){
+  mx <- dataframe$Nati/dataframe$Pop  #age-specific fertility rates
+  nLx<-lifetable$nLx
+  p_x<-numeric(0)
+  for (i in 1:(length(nLx)-1)) {p_x<-c(p_x,nLx[i+1]/nLx[i])}
+  phi_x<-numeric(0)
+  for (i in 1:(length(nLx)-1)) {phi_x<-c(phi_x,nLx[1]/2*(mx[i]+p_x[i]*mx[i+1]))}
+  phi_x<-c(phi_x,0)
+  lesl_mat<-matrix(0, length(lifetable$nLx),length(lifetable$nLx))
+  lesl_mat[1,]<-phi_x
+  for (i in 1:(length(lifetable$nLx)-1)) {lesl_mat[(i+1),i]<-p_x[i]}
+  return(lesl_mat)}
+
+
+
+
+
+Predicted_pop_leslie<-function(leslie_mat,Population,starting_year,years){
+  pop_prev<-numeric(0)
+  time<-years-starting_year
+  for (i in time) {pop_prev<-c(pop_prev,sum(matrix.power(lesl_mat,i)%*%Population))}
+  return(data.frame(pop_prev,years))}
